d22494496eb4b34e85dfadb7e9b11233b51d9022
Vaclav Klecanda <vencax77@gmail.com>
2016-07-13 20:00:43 +0200

    changed

--- test-unit-single.sh
+++ test-unit-single.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-if [ ! -d ./src ]
-then
-    touch jest.lastrun
-    ./node_modules/babel/bin/babel/index.js app --out-dir src --stage 1 --compact false > /dev/null
-else
-    for i in `find app -type f -newer jest.lastrun`
-    do
-        ./node_modules/babel/bin/babel/index.js $i --out-file `echo $i | sed 's#^app#src#'`
-    done
-    touch jest.lastrun
-fi
-
-./node_modules/jest-cli/bin/jest.js src/$1
+./node_modules/.bin/jest --runInBand--runInBand --testPathPattern $1
