baf8c350d8398c4a356f29c70a54bf9513411b9b
Vaclav Klecanda <vencax77@gmail.com>
2016-10-14 20:02:25 +0200

    login view

--- examples/blog/js/components/login.js
+++ examples/blog/js/components/login.js
@@ -1,47 +1,68 @@
-import React from 'react';
-import { browserHistory } from 'react-router';
+import React from 'react'
+import { browserHistory } from 'react-router'
+import { Card, CardTitle, CardActions } from 'material-ui/Card'
+import TextField from 'material-ui/TextField'
+import RaisedButton from 'material-ui/RaisedButton'
+import { observer } from 'mobx-react'
+import { observable, toJS } from 'mobx'
 
-export default class Login extends React.Component {
+@observer
+class LoginForm extends React.Component {
 
-  constructor() {
-    super();
-    this.state = {
-      uname: '',
-      pwd: ''
-    };
+  static propTypes = {
+    onLoginSubmitted: React.PropTypes.func.isRequired
   }
 
-  unameChange(e) {
-    e.preventDefault();
-    this.setState({uname: e.target.value, pwd: this.state.pwd});
+  @observable credentials = {
+    uname: '',
+    pwd: ''
   }
+  @observable submitted = false
 
-  pwdChange(e) {
-    e.preventDefault();
-    this.setState({uname: this.state.uname, pwd: e.target.value});
-  }
-
-  login(e) {
-    e.preventDefault();
-    this.props.state.login(this.state).then((user) => {
-      browserHistory.push('/');
-    });
+  handleSubmit(e) {
+    e.preventDefault()
+    this.submitted = true
+    this.props.onLoginSubmitted(toJS(this.credentials)).catch((err)=>{
+      this.submitted = false
+    })
   }
 
   render() {
     return (
-      <form role='form'>
-        <div className='form-group'>
-          <input type='text' value={this.state.uname}
-            onChange={this.unameChange.bind(this)}
-            placeholder='Username' />
-          <input type='password' value={this.state.password}
-            onChange={this.pwdChange.bind(this)}
-            placeholder='Password' />
-        </div>
-        <button type='submit' onClick={this.login.bind(this)}>Submit</button>
-      </form>
-    );
+      <Card style={{ margin: '1em'}}>
+        <CardTitle title={'login'} />
+
+        <form style={{ padding: '0 1em 1em 1em' }}>
+          <TextField name={'uname'} floatingLabelText={'uname'}
+            value={this.credentials.uname} onChange={(e)=>{this.credentials.uname = e.target.value}} />
+          <br/>
+          <TextField name={'pwd'} floatingLabelText={'pwd'} type={'password'}
+            value={this.credentials.pwd} onChange={(e)=>{this.credentials.pwd = e.target.value}} />
+        </form>
+
+        <CardActions>
+          <RaisedButton onTouchTap={this.handleSubmit.bind(this)} label={'login'} disabled={this.submitted} />
+        </CardActions>
+      </Card>
+    )
+  }
+
+}
+
+export default class LoginView extends React.Component {
+
+  static propTypes = {
+    state: React.PropTypes.object.isRequired
+  }
+
+  login(credentials) {
+    return this.props.state.login(credentials).then((user) => {
+      browserHistory.push('/')
+    })
+  }
+
+  render() {
+    return <LoginForm onLoginSubmitted={this.login.bind(this)} />
   }
 
 }
