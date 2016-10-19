e5e9ace02c0ecb7d135aaf573d3fa59a6c7496b2
Vaclav Klecanda <vencax77@gmail.com>
2016-10-19 06:58:40 +0200

    librarization round 1

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,42 @@
 var debug = process.env.NODE_ENV !== "production"
 var webpack = require('webpack')
 var path = require('path')
+var libraryName = 'react-mobx-admin'
 
 module.exports = {
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: "./examples/blog/js/main.js",
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-      }
-    ]
-  },
+  entry: __dirname + '/src/index.js',
+  devtool: 'source-map',
   output: {
-    path: path.join(__dirname, "examples/list_test"),
-    filename: "main.min.js"
+    libraryTarget: 'umd',
+    library: libraryName,
+    path: __dirname + '/lib',
+    filename: libraryName + '.js'
   },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
+  externals: {
+    'mobx-react': {
+      root: 'mobxReact',
+      commonjs: 'mobx-react',
+      commonjs2: 'mobx-react',
+      amd: 'mobx-react'
+    },
+    'react': {
+      root: 'React',
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react'
+    },
+    mobx: 'mobx',
+    axios: 'axios',
+    'material-ui': 'material-ui'
+  }
 }
